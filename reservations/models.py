892c3da042c067f1071237bb99536b72f0818548
celeste-6 <sage.celeste6@gmail.com>
2022-05-09 17:59:41 +0900

    create: models.py for core, users, reservations, reviews, and restaurants

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db          import models
+from core.models        import TimeStampedModel
+from users.models       import User
+from restaurants.models import Restaurant
 
-# Create your models here.
+class Reservation(TimeStampedModel):
+    restaurant    = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user          = models.ForeignKey(User, on_delete=models.CASCADE)
+    status        = models.ForeignKey('ReservationStatus', on_delete=models.CASCADE)
+    date          = models.DateField()
+    start_time    = models.TimeField()
+    end_time      = models.TimeField()
+    visitor_count = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'reservations'
+
+class ReservationStatus(models.Model):
+    status = models.CharField(max_length=100)
+
+    class Meta:
+        db_table ='reservation_statuses'
